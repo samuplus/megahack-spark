f15d271f46184780bdd43e52a0d8339574b08817
Evellyn Lima <evellynlrl@hotmail.com>
2020-05-02 15:32:50 -0300

    Add connection to fortmatic

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,28 @@
-import Web3 from 'web3'
 import React, { useEffect } from 'react'
+import Web3 from 'web3'
+import Fortmatic from 'fortmatic'
+
+const fortmaticKey = process.env.REACT_APP_FORMATIC_TEST
 
 const Home = () => {
   useEffect(() => {
-    const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545')
-    console.log({ web3 })
+    getAccounts()
   }, [])
+
+  const getAccounts = async () => {
+    try {
+
+      debugger
+      const fm = new Fortmatic(fortmaticKey)
+      const web3 = new Web3(fm.getProvider())
+
+      const response = await web3.eth.getAccounts()
+      console.log({ response })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return <h1>MegaHack - Spark</h1>
 };
 
