ec3e066224b7db83e7f9be111c5e3ce59fc0fd0f
Evellyn Lima <evellynlrl@hotmail.com>
2020-05-03 12:38:00 -0300

    Fix initial values

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -13,38 +13,50 @@ import Button from 'components/Button'
 
 import * as S from './styles'
 
-const Cadastro = () => (
-  <S.Content>
-    <HeaderLogout />
+const Cadastro = () => {
+  const initialValues = {
+    email: '',
+    cpf: '',
+    phone: '',
+    birthdate: '',
+    humanrace: '',
+    gender: '',
+    cep: '',
+    terms: false
+  }
+  return (
+    <S.Content>
+      <HeaderLogout />
 
-    <S.CadastroContainer>
-      <S.Title>Vamos começar a cuidar da sua saúde?</S.Title>
+      <S.CadastroContainer>
+        <S.Title>Vamos começar a cuidar da sua saúde?</S.Title>
 
-      <S.FormWrapper>
-        <Formik initialValues={{ email: '', password: '' }} onSubmit={() => console.log('submit')}>
-          {({ isSubmitting }) => (
-            <Form>
-              <Input name="name" type="text" label="Nome completo:" />
-              <Input name="cpf" type="text" label="CPF:" />
-              <Input name="phone" type="text" label="Telefone:" />
-              <Input name="birthdate" type="text" label="Data de nascimento:" />
-              <Input name="human-race" type="text" label="Raça:" />
-              <Input name="gender" type="text" label="Gênero:" />
-              <Input name="cep" type="text" label="CEP:" />
+        <S.FormWrapper>
+          <Formik initialValues={initialValues} onSubmit={(values) => console.log({ values })}>
+            {({ isSubmitting }) => (
+              <Form>
+                <Input name="name" type="text" label="Nome completo:" />
+                <Input name="cpf" type="text" label="CPF:" />
+                <Input name="phone" type="text" label="Telefone:" />
+                <Input name="birthdate" type="text" label="Data de nascimento:" />
+                <Input name="humanrace" type="text" label="Raça:" />
+                <Input name="gender" type="text" label="Gênero:" />
+                <Input name="cep" type="text" label="CEP:" />
 
-              <Checkbox name="terms" label="Eu li e concordo com os termos de uso" />
+                <Checkbox name="terms" label="Eu li e concordo com os termos de uso" />
 
-              <S.FormButtonWrapper>
-                <Button theme="secondary" type="submit" disabled={isSubmitting}>
-                  Finalizar cadastro
-                </Button>
-              </S.FormButtonWrapper>
-            </Form>
-          )}
-        </Formik>
-      </S.FormWrapper>
-    </S.CadastroContainer>
-  </S.Content>
-)
+                <S.FormButtonWrapper>
+                  <Button theme="secondary" type="submit" disabled={isSubmitting}>
+                    Finalizar cadastro
+                  </Button>
+                </S.FormButtonWrapper>
+              </Form>
+            )}
+          </Formik>
+        </S.FormWrapper>
+      </S.CadastroContainer>
+    </S.Content>
+  )
+}
 
 export default Cadastro
