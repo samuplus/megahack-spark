a5124e7e606508de81e62d1b024be97faa4fff36
Evellyn Lima <evellynlrl@hotmail.com>
2020-05-04 18:30:39 -0300

    Add dialog flow script

--- src/components/Cards/TalkToDoctorCard/styles.js
+++ src/components/Cards/TalkToDoctorCard/styles.js
@@ -3,18 +3,34 @@ import styled from 'styled-components'
 import colors from 'common/colors';
 import CardBase from 'components/Cards'
 
-export const Card = styled(CardBase)``
+export const Card = styled(CardBase)`
+`
 
 export const CardMessage = styled.h2`
+  bottom: 0;
   background-color: ${colors.blue};
   color: white;
   font-size: 18px;
+  height: 40px;
+  left: 0;
   line-height: 20px;
   font-weight: normal;
   padding: 10px;
+  position: absolute;
+  width: 100%;
+  z-index: 5;
 `
 
-export const CardTop = styled.div``
+export const CardTop = styled.div`
+  display: flex;
+  height: 220px;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    width: 100%;
+  }
+`
 
 export const CardBottom = styled.div`
   align-items: center;
